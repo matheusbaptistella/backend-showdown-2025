37039de439e4faa203fc2090de93e6c0eb919e36
Matheus Baptistella <mbapts@gmail.com>
2025-07-24 18:02:15 -0300

    apply cargo-fmt andf update readme

--- README.md
+++ README.md
@@ -5,3 +5,7 @@ This is my submission for the "Rinha de Backend 2025" (2025 Backend Showdown) co
 ## Development
 
 We'll start by developing the simplest application that can handles the requests/responses and processing of the data correctly. After that, the idea is to use our knowledge of computer architecture concepts like memory management or even 3rd party tools like Tokio's Tracing library, to identiify performance bottlenecks and improve them!
+
+To get things started our objective will be to deploy one instance of the Rust web backend with a Redis instance and an Nginx load-balancer. Also, we will limit ourselves to using alway the default Payment Processor instance. Clearly not the best scenario but we want to track our improvement.
+
+Next we will craft an algorithm to select between instances of the Payment Processor. After that we can experiment with 2  instances of the backend web server. Finally, we can use an in-memory in-process db. Since we want to achieve the best performance, regardless of concepts like availability and persistency of data in our backend, the fatest access we can get is storing things in the same process so we avoid serialization and network overhead when using Redis (we'll see how much we can save by doing this).
--- src/main.rs
+++ src/main.rs
@@ -1,65 +1,61 @@
 use axum::{
+    Json, Router,
     extract::{Query, State},
     routing::{get, post},
-    Json, Router
 };
 use chrono::{DateTime, Utc};
-use serde::{Deserialize, Serialize};
 use reqwest::Client;
-use std::{collections::HashMap, sync::Arc};
-use tokio::sync::RwLock;
-
-
-#[derive(Clone)]
-struct AppState {
-    client: Client,
-    db: Arc<RwLock<HashMap<String, Data>>>,
-}
-
+use serde::{Deserialize, Serialize};
 
 struct Data {
-    data: (DateTime<Utc>, f64)
+    data: (DateTime<Utc>, f64),
 }
-
 
 #[tokio::main]
 async fn main() {
     let client = Client::new();
-    let state = Arc::new(AppState {
-        client: client,
-        db: Arc::new(RwLock::new(HashMap::new())),
-    });
 
     let app = Router::new()
         .route("/payments", post(payments))
         .route("/payments-summary", get(payments_summary))
-        .with_state(state);
+        .with_state(client);
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:9999").await.unwrap();
 
     axum::serve(listener, app).await.unwrap();
 }
 
-
-async fn payments(State(state): State<Arc<AppState>>, Json(cp): Json<CreatePayment>) {
-    let mp = MakePayment {
+async fn payments(State(client): State<Client>, Json(cp): Json<CreatePayment>) {
+    let rp = RequestPayment {
         create_payment: cp,
         requested_at: Utc::now(),
     };
 
-    let _ = state.client.clone().post("http://payment-processor-default:8080/payments").json(&mp).send().await.unwrap();
+    let _ = client
+        .clone()
+        .post("http://payment-processor-default:8080/payments")
+        .json(&rp)
+        .send()
+        .await
+        .unwrap();
 }
 
-
-async fn payments_summary(Query(_): Query<PaymentsSummaryQueryParams>) -> Json<PaymentProcessorsSummaries> {
+async fn payments_summary(
+    Query(_): Query<PaymentsSummaryQueryParams>,
+) -> Json<PaymentProcessorsSummaries> {
     let summary = PaymentProcessorsSummaries {
-        default_sum: Summary { total_requests: 0, total_amount: 0.0 },
-        fallback: Summary { total_requests: 0, total_amount: 0.0 }
+        default_sum: Summary {
+            total_requests: 0,
+            total_amount: 0.0,
+        },
+        fallback: Summary {
+            total_requests: 0,
+            total_amount: 0.0,
+        },
     };
 
     Json(summary)
 }
-
 
 #[derive(Deserialize, Serialize)]
 struct CreatePayment {
@@ -68,22 +64,19 @@ struct CreatePayment {
     amount: f64,
 }
 
-
 #[derive(Serialize)]
-struct MakePayment {
+struct RequestPayment {
     #[serde(flatten)]
     create_payment: CreatePayment,
     #[serde(rename = "requestedAt")]
     requested_at: DateTime<Utc>,
 }
 
-
 #[derive(Deserialize)]
 struct PaymentsSummaryQueryParams {
     from: Option<DateTime<Utc>>,
     to: Option<DateTime<Utc>>,
 }
-
 
 #[derive(Serialize)]
 struct PaymentProcessorsSummaries {
@@ -92,11 +85,10 @@ struct PaymentProcessorsSummaries {
     fallback: Summary,
 }
 
-
 #[derive(Serialize)]
 struct Summary {
     #[serde(rename = "totalRequests")]
     total_requests: u32,
     #[serde(rename = "totalAmount")]
-    total_amount:f64,
+    total_amount: f64,
 }
